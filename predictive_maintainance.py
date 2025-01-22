387da8ca26d1bcf15b7d7e13c1caa51f1387b099
manishmmj <164055745+manishmmj@users.noreply.github.com>
2025-01-22 18:54:49 +0530

    Update predictive_maintainance.py

--- predictive_maintainance.py
+++ predictive_maintainance.py
@@ -5,7 +5,7 @@ import pickle
 
 
 # Load the pre-trained model and scaler
-MODEL_PATH = "model_logistic_reg.pkl"  # Update with the saved model path
+MODEL_PATH = "model_XgbClassifier.pkl"  # Update with the saved model path
 
 # load using pickle
 with open(MODEL_PATH, "rb") as model_file:
